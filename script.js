b44c46127e35dc248d10974a468b48d384ddac3f
unknown <belloahdamkehinde@gmail.com>
2025-11-25 16:40:48 -0600

    working on book call

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 let navLinks = document.querySelector (".nav-link");
 let menuLinks = document.querySelector("#menu-links");
 
-menuLinks.addEventListener("click", function(){
+menuLinks.addEventListener("click", function (){
   navLinks.classList.toggle("active")
+});
+
+
+let  bookMeeting = document.querySelector(".book-meeting");
+console.log(bookMeeting)
+bookMeeting.addEventListener("click", function(){
+  alert("Opening meeting scheduler…")
+
+  setTimeout(() => {
+    alert("Here you can choose a date & time!");
+  }, 1000);
+}) 
+
+let fullName = document.querySelector("#full-name");
+let companyName = document.querySelector("#company-name");
+let email = document.querySelector("#email");
+let phone =  document.querySelector("#phone-number")
+let submitform = document.querySelector(".form-sections")
+let valid = true
+console.log(submitform)
+submitform.addEventListener("submit", function(e){
+  if(fullName.value.trim() === ""){
+    fullName.style.borderColor="red";
+    valid = false;
+  }
 })
+
+
